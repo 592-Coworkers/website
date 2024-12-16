3db685b79855ac7798d21fabb8625e966ae5ae68
Ian Kranz <87909530+iankranz@users.noreply.github.com>
2024-12-15 19:32:59 -0600

    create quiz context (#4)

---
+++ src/holiday-2024.js
@@ -0,0 +1,37 @@
+import { createApp } from "vue"
+import App from "./quiz/App.vue"
+
+const quizApp = document.querySelector("#quiz-app")
+const snowFAB = document.querySelector("#snow-fab")
+const quizButton = document.querySelector("#quiz-button")
+
+function snowSome() {
+  confetti({
+    angle: 270,
+    colors: ["#ffffff"],
+    drift: 0.3,
+    flat: true,
+    gravity: 0.5,
+    origin: { x: 0.4, y: -0.2 },
+    scalar: 0.6,
+    shapes: ["circle"],
+    spread: 180,
+    startVelocity: 25,
+    ticks: 600
+  })
+}
+
+function showQuizApp() {
+  quizApp.classList.remove("hidden")
+}
+
+function hideQuizApp() {
+  quizApp.classList.add("hidden")
+}
+
+const app = createApp(App).mount("#quiz-app")
+
+snowFAB.addEventListener("click", snowSome)
+quizButton.addEventListener("click", showQuizApp)
+
+export { hideQuizApp }
